eb9fc184d59d59c8e90144a55fe87cdde4b23290
Jason Palma <jasonpalma16@gmail.com>
2024-10-07 20:51:34 -0400

    Define getLength and default constructor
    Note that you should comment out the destructor to allow compilation of the code at this state.

--- main.cpp
+++ main.cpp
@@ -32,9 +32,58 @@ public:
 
 };
 
+template <class T>
+int CLList<T>::getLength() const { return length; }
+template <class T>
+void CLList<T>::addItem(T item) {
+    // TODO: Define function
+}
+template <class T>
+void CLList<T>::deleteItem(T item) {
+    // TODO: Define function
+}
+template <class T>
+void CLList<T>::printList() const {
+    // TODO: Define function
+}
+template <class T>
+bool CLList<T>::searchItem(T item) const {
+    // TODO: Define function
+}
+template <class T>
+bool CLList<T>::isEmpty() const {
+    // TODO: Define function
+}
+template <class T>
+void CLList<T>::destroy() {
+    // TODO: Define function
+}
+template <class T>
+void CLList<T>::copy() {
+    // TODO: Define function
+}
+
+
+template <class T>
+CLList<T>::CLList() : first(nullptr), length(0) {}
+template <class T>
+CLList<T>::~CLList() {
+    // TODO: Define function
+}
+template <class T>
+CLList<T>::CLList(const CLList<T> & right) {
+    // TODO: Define function
+}
+template <class T>
+const CLList<T> & CLList<T>::operator=(const CLList<T> & right) {
+    // TODO: Define function
+}
+
 
 int main() {
     CLList<int> list;
+
+    cout << "Length: " << list.getLength() << endl;
 
     return 0;
 }
