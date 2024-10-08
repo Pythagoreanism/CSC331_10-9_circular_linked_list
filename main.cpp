69f1a64980b318d72be4d59a752521344ec266d0
Jason Palma <jasonpalma16@gmail.com>
2024-10-07 20:30:24 -0400

    Create and define class CCList
    Also fixed declaration of item in struct node

--- main.cpp
+++ main.cpp
@@ -8,6 +8,31 @@ struct node {
     node<T>* next;
 };
 
+
+template <class T>
+class CCList {
+private:
+    node<T>* first;
+    int length;
+
+public:
+    int getLength() const;
+    void addItem(T);
+    void deleteItem(T);
+    void printList() const;
+    bool searchItem(T) const;
+    bool isEmpty() const;
+
+
+
+    CCList();
+    ~CCList();
+    CCList(const & CCList<T>);
+    const & CCList<T> operator=(const & CCList<T>);
+
+};
+
+
 int main() {
     cout << "Hello world" << endl;
 
